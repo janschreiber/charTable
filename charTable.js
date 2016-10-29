f35bc302cff0df01d2fe93566016acc6032ae3b2
Jan Schreiber <jan.schreiber@languagetool.org>
2016-10-29 21:45:02 +0200

    Work in progress

--- charTable.js
+++ charTable.js
@@ -1,10 +1,13 @@
+"use strict";
+
 function openCharTable {
     var s = "’…@áÁàÀâÂäÄåÅæÆçÇéÉèÈêÊëË€əğĞİîÎïÏıñÑňŇôÔöÖœŒøØßşŞúÚùÙûÛüÜýÝỳỲÿŸžŽ",
         character = "",
-        charcode = 0;
+        charcode = 0,
+        table = document.getElementById("charTable");
+
     for (var i = 0; i < s.length; i++) {
         character = s.charAt(i);
         charcode = s.charCodeAt(i);
-        table = document.getElementById("charTable");
     }
-}    
+}
